865e2d13e4206ef66d73374488ae84f54bfe6db2
SoftBlade <94767111+SoftBlade7@users.noreply.github.com>
2026-09-01 22:30:51 +0100

    Update main.cpp

--- aurora-main/lib/main.cpp
+++ aurora-main/lib/main.cpp
@@ -1,6 +1,5 @@
 #include <aurora/main.h>
-#undef main
 
 #include <SDL3/SDL_main.h>
 
-int main(int argc, char** argv) { return aurora_main(argc, argv); }
+int SDL_main(int argc, char** argv) { return aurora_main(argc, argv); }
